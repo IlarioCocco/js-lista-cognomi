10533fdaddb0ea17e3162e240e3ea44e6995a711
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-26 17:14:25 +0200

    commiti 2

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// 1 chiedi all'utente il suo Cognome:
+var cognome= prompt("inserisci il tuo Cognome");
+console.log("Cognome:" + " " + cognome);
+alert("Cognome:" + " " + cognome);
+
+// 2 Inseriscilo in un array con altri cognomi:
+var ListaCognomi= ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+ListaCognomi.push(cognome);
+console.log(ListaCognomi);
+alert("Lista aggiornata :" +  " " + ListaCognomi);
+
+// 3 Stampa la lista ordinata in sequenza Alfabetica:
+ListaCognomi.sort();
+console.log(ListaCognomi);
+alert("Lista in Ord. Alfabetico :" + " " + ListaCognomi);
+
+//  4 Scrivi anche la posizione "umana" partendo da 1, 
+//  della lista in cui il nuovo utente si trova
+for (var i= 1; i< ListaCognomi.length; i++){
+    // document.write(ListaCognomi[i] + "<br>");
+    console.log(ListaCognomi[i]);
+    alert(ListaCognomi[i]);
+}
