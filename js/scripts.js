367d3e8e974a8cf6f85692daf99b76103e2bc27c
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-26 19:57:27 +0200

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 function minuscoleMaiuscole(a, b) {
     minA = a.toLowerCase();
     minB = b.toLowerCase();
-    if (minA < minB) { return -1; }
-    else {
-        if (minA > minB) { return 1; }
-        else { return 0; }
-    }
+    if (minA < minB){ return -1; }
+    else if (minA > minB) { return 1;}
+    else { return 0; }
 }
 
 
@@ -28,7 +26,7 @@ alert("Lista in Ord. Alfabetico :" + " " + ListaCognomi);
 //  4 Scrivi anche la posizione "umana" partendo da 1, 
 //  della lista in cui il nuovo utente si trova
 for (var i= 1; i< ListaCognomi.length; i++){
-    console.log(ListaCognomi[i]);
-    alert(ListaCognomi[i]);
-    // document.write(ListaCognomi[i] + "<br>");
+    console.log(cognome);
+    document.write(ListaCognomi[i] + "<br>");
 }
+
