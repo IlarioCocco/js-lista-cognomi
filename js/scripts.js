d6a535ae149cc0e20ac05e6658c07034b3b48690
IlarioCOCCO <ilario.cocco@gmail.com>
2021-08-26 17:46:58 +0200

    commit 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+function minuscoleMaiuscole(a, b) {
+    minA = a.toLowerCase();
+    minB = b.toLowerCase();
+    if (minA < minB) { return -1; }
+    else {
+        if (minA > minB) { return 1; }
+        else { return 0; }
+    }
+}
+
+
 // 1 chiedi all'utente il suo Cognome:
 var cognome= prompt("inserisci il tuo Cognome");
 console.log("Cognome:" + " " + cognome);
@@ -10,14 +21,14 @@ console.log(ListaCognomi);
 alert("Lista aggiornata :" +  " " + ListaCognomi);
 
 // 3 Stampa la lista ordinata in sequenza Alfabetica:
-ListaCognomi.sort();
+ListaCognomi.sort(minuscoleMaiuscole);
 console.log(ListaCognomi);
 alert("Lista in Ord. Alfabetico :" + " " + ListaCognomi);
 
 //  4 Scrivi anche la posizione "umana" partendo da 1, 
 //  della lista in cui il nuovo utente si trova
 for (var i= 1; i< ListaCognomi.length; i++){
-    // document.write(ListaCognomi[i] + "<br>");
     console.log(ListaCognomi[i]);
     alert(ListaCognomi[i]);
+    // document.write(ListaCognomi[i] + "<br>");
 }
